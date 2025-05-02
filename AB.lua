c8c8e178d992313b3ee7cd2a214375c28eaa5fe8
acediamiau <drakerealpaul@gmail.com>
2025-05-01 23:50:14 -0400

    Add files via upload

--- AB.lua
+++ AB.lua
@@ -19,35 +19,9 @@ local Section = Tab:AddSection({
 	Name = "test"
 })
 
-Tab:AddButton(
+Tab:AddButton()
 	Name = "tweening test",
-	Callback = function(function giveTeleportTool(player)
-        local tool = Instance.new("Tool")
-        tool.Name = "TeleportTool"
-        tool.RequiresHandle = true
-        
-        local handle = Instance.new("Part")
-        handle.Name = "Handle"
-        handle.Size = Vector3.new(1, 1, 1)
-        handle.Anchored = false
-        handle.CanCollide = false
-        handle.BrickColor = BrickColor.new("Bright blue")
-        handle.Parent = tool
-        
-        tool.Parent = player.Backpack
-    
-        tool.Activated:Connect(function()
-            if player.Character then
-                teleportToRandomLocation(player)
-            end
-        end)
-    end
-    
-    game.Players.PlayerAdded:Connect(function(player)
-        player.CharacterAdded:Connect(function(character)
-            giveTeleportTool(player)
-        end)
-    end)
-    )    
+	Callback = function(loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))())
+  
       		print("button pressed")
 OrionLib:Init()
