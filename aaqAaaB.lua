afbcbf0eb173e51bee8496660f3426f49cf684cc
acediamiau <drakerealpaul@gmail.com>
2025-05-01 23:59:59 -0400

    Add files via upload

---
+++ aaqAaaB.lua
@@ -0,0 +1,25 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Orion Library Template", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "hi anything for me",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function(loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))())
+      		print("loading")
+  	end    
+})
+
+OrionLib:Init()
